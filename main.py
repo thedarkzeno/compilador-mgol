77f0f5c1dd10106b424b16310e6a38750bf7fea3
thedarkzeno <multiolx@gmail.com>
2024-07-13 22:33:45 -0300

    create data dir

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 if __name__ == "__main__":
 
-    gramatica = pd.read_csv("./sintatico/gramatica.csv")
-    action_table = pd.read_csv("./sintatico/action.csv")
-    goto_table = pd.read_csv("./sintatico/goto.csv")
+    gramatica = pd.read_csv("./data/gramatica.csv")
+    action_table = pd.read_csv("./data/action.csv")
+    goto_table = pd.read_csv("./data/goto.csv")
 
     scanner = Scanner()
     parser = Parser(action_table, goto_table, gramatica, scanner)
 
-    with open("lexico/teste.mgol") as file:
+    with open("data/teste.mgol") as file:
         codigo = file.read()
 
     parser.parse(codigo)
